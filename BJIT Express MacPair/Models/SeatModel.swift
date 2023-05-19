67942bb95c661be699fe2f51f1d0e3d3211e6351
arafatshahed <arafatyeasir94@gmail.com>
2023-05-19 16:03:00 +0600

    added seatmodel

--- BJIT Express MacPair/Models/SeatModel.swift
+++ BJIT Express MacPair/Models/SeatModel.swift
@@ -6,8 +6,38 @@
 //
 
 import Foundation
+import CloudKit
 
-@MainActor
-class SeatModel: ObservableObject{
+enum SeatModelKeys: String {
+    case type = "CKSeatModel"
+}
+
+struct SeatModel {
+    var recordId: CKRecord.ID?
+    var seatNumber: String
+    var bookedBy: String
+    var isReserved: Bool = false
+    var isFilled: Bool = false
     
+    var record: CKRecord {
+        let record = CKRecord(recordType: SeatModelKeys.type.rawValue)
+        record["seatNumber"] = seatNumber
+        record["bookedBy"] = bookedBy
+        record["isReserved"] = isReserved
+        record["isFilled"] = isFilled
+        return record
+    }
+}
+
+extension SeatModel {
+    init?(record: CKRecord) {
+        
+        guard let seatNumber = record["seatNumber"] as? String,
+              let bookedBy = record["bookedBy"] as? String,
+              let isReserved = record["isReserved"] as? Bool,
+              let isFilled = record["isFilled"] as? Bool else {
+            return nil
+        }
+        self.init(recordId: record.recordID, seatNumber: seatNumber, bookedBy: bookedBy, isReserved: isReserved, isFilled: isFilled)
+    }
 }
